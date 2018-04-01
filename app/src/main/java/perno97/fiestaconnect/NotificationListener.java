6fc75e55d9040f4e1310003d88470f7d35cf22fb
perno97 <fabio.perini97@gmail.com>
2018-04-01 19:25:50 +0200

    Mostra notifiche di whatsapp.

--- app/src/main/java/perno97/fiestaconnect/NotificationListener.java
+++ app/src/main/java/perno97/fiestaconnect/NotificationListener.java
@@ -9,25 +9,29 @@ import android.widget.Toast;
 public class NotificationListener extends NotificationListenerService {
 
     private String TAG = "notificationListener";
+    public static final String WHATSAPP_PACK_NAME = "com.whatsapp";
 
     @Override
     public void onListenerConnected() {
         super.onListenerConnected();
         Log.e(TAG,"NOTIFICATION LISTENER CONNECTED");
-        Toast.makeText(this, "NOTIFICATION LISTENER CONNECTED", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, R.string.toastNotList, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         Log.e(TAG, "NOTIFICATION RECEIVED");
-        Toast.makeText(this, "NOTIFICATION RECEIVED", Toast.LENGTH_SHORT).show();
-        startService(SdlService.getIntent(getApplicationContext(), "Notifica"));
-        /*Bundle extras = sbn.getNotification().extras;
+        if(sbn.getPackageName().equals(WHATSAPP_PACK_NAME))
+            showNotification(sbn);
+    }
+
+    private void showNotification(StatusBarNotification sbn) {
+        Bundle extras = sbn.getNotification().extras;
         String title = extras.getString("android.title");
         String text;
         if(extras.getCharSequence("android.text") != null) {
             text = extras.getCharSequence("android.text").toString();
-            startService(SdlService.getIntent(getApplicationContext(), "Titolo: " + title + ". Testo: " + text));
-        }*/
+            startService(SdlService.getIntent(getApplicationContext(), "Titolo: " + title + ". \nTesto: " + text));
+        }
     }
 }
